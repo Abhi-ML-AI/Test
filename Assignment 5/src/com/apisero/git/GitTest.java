4842ba4cccf1241e27ffbd6226c38183e89b20be
Naman Chaturvedi <55281831+Spartannaman@users.noreply.github.com>
2021-08-06 15:36:46 +0530

    Updating the repo...

--- Assignment 5/src/com/apisero/git/GitTest.java
+++ Assignment 5/src/com/apisero/git/GitTest.java
@@ -5,6 +5,8 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is a github demo!!");
+		System.out.println("Hello from Spartannaman");
+		System.out.println("Hello 2");
 	}
 
 }
